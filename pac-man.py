7bc57f1d7d95c36ef5fe303524e8af0d5cba0a0b
ralrawaj <ralrawaj@c1r2s4.irbid.42.school>
2026-09-13 12:26:22 +0300

    protect the program from KeyboardInterrupt error

--- pac-man.py
+++ pac-man.py
@@ -21,18 +21,20 @@ pygame.init()
 screen = pygame.display.set_mode((1600, 900))
 frame_clock = pygame.time.Clock()
 mode = "menu"
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    # show_score(screen=screen, conf=configuration)
-    # log_in_secreen(screen=screen,
-    #                frame_clock=frame_clock,
-    #                conf=configuration)
-    show_menu(
-        screen=screen,
-        cofiguration=configuration,
-        fram_clock=frame_clock)
-    pygame.display.update()
-    frame_clock.tick(60)
+try:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # show_score(screen=screen, conf=configuration)
+        # log_in_secreen(screen=screen,
+        #                frame_clock=frame_clock,
+        #                conf=configuration)
+        show_menu(
+            screen=screen,
+            cofiguration=configuration,
+            fram_clock=frame_clock)
+        pygame.display.update()
+        frame_clock.tick(60)
+except KeyboardInterrupt:
+    print("\nError: Stop the program from the termonal using ctrl + c !!")
